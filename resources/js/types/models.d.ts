a98c5f4c5edc66ef17bbb92d803ef49b7eea6dda
=hesitorremitos <hesitorremitos@gmail.com>
2025-08-19 14:07:29 -0400

    feat: add current page and pagination details to PaginatedResponse interface

--- resources/js/types/models.d.ts
+++ resources/js/types/models.d.ts
@@ -118,3 +118,23 @@ export interface DiplomaPageProps {
   facultades?: Facultad[]
   carreras?: Carrera[]
 }
+
+export interface PaginatedResponse<T> {
+    data: T[];
+    links: {
+        url: string | null;
+        label: string;
+        active: boolean;
+    }[];
+    from: number;
+    to: number;
+    total: number;
+    current_page: number;
+    last_page: number;
+    first_page_url: string;
+    last_page_url: string;
+    next_page_url: string | null;
+    prev_page_url: string | null;
+    path: string;
+    per_page: number;
+}
